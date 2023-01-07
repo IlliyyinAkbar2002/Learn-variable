59dc957c7502ef8a5de07f3998db90436632bff1
Illiyyin Akbar Ariyanto <58885451+IlliyyinAkbar2002@users.noreply.github.com>
2023-01-07 15:59:16 +0700

    Create file main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Learn create variables
+# Create variable
+# And call variable to print function
+
+name = "jack"
+print(name)
+
+name = "Illiyyin"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
